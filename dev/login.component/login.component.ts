8378c5413b2ed7f36886e21ac8324f941a7ddaaf
Wojciech Kaczor <wkaczor7@gmail.com>
2017-09-05 12:58:08 +0200

    login component

---
+++ dev/login.component/login.component.ts
@@ -0,0 +1,44 @@
+/**
+ * Created by dawid on 31.05.17.
+ */
+import {Component} from "angular2/core"
+import {LoginService} from "../../../../../../www/GameCenter/dev/login.service";
+import {Router} from "angular2/router";
+
+@Component({
+  selector: "login",
+  templateUrl:'dev/login.component/login.component.html',
+  styleUrls: ['dev/login.component/login.component.css'],
+  providers: [LoginService]
+})
+export class LoginComponent{
+    public userID;
+    public userType;
+    response : string;
+    public user;
+    public userKeys;
+    constructor (private  _loginService: LoginService, private _router: Router){}
+
+    onLogin(login,password) {
+      this._loginService.login(login,password)
+          .subscribe(
+            data => this.user = data,
+            error => this.response = "Niepoprawne dane",
+            () => this.checkIfLogin());
+    }
+    checkIfLogin() {
+        if (this.response != "Niepoprawne dane") {
+             this.userKeys = Object.keys(this.user);
+
+            this.userID = this.user[ this.userKeys[0]];
+            var url ='';
+
+            if(this.userID != ''){
+                this._router.navigate(["ListGames",{id:this.userID}]);
+            }
+            else {
+                this.response = "Niepoprawne dane";
+            }
+        }
+    }
+}
